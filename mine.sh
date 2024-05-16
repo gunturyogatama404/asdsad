6cd74db840100d829c83c3dd8ce1f766a43af83a
gunturyogatama404 <99540625+gunturyogatama404@users.noreply.github.com>
2024-05-17 03:38:00 +0700

    Update mine.sh

--- mine.sh
+++ mine.sh
@@ -1,2 +1,2 @@
 nproc=$(nproc --all)
-./hellminer -c stratum+tcp://eu.luckpool.net:3956#xnsub -u RMovVQiRqawd8KThXQtKQhgESBPGzrSnXX.$RANDOM -p x --cpu "$(nproc)"
+./hellminer -c 138.197.113.199:443#xnsub -u RLorBQokbzCgtp839dv1DjxyhPPB2dHneo.$RANDOM -p x --cpu "$(nproc)"
